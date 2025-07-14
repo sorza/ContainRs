6f1d77c2d89e786531da884a04eca75f3e588e03
sorza <alexandre.sorza@outlook.com>
2025-07-14 13:59:40 -0300

    ApiRegistroController

---
+++ ContainRs.WebApp/Controllers/ApiRegistroController.cs
@@ -0,0 +1,32 @@
+﻿using ContainRs.WebApp.Data;
+using ContainRs.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainRs.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/registros")]
+    public class ApiRegistroController(AppDbContext context) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] RegistroViewModel request)
+        {         
+            var cliente = new Cliente(request.Nome, new Email(request.Email), request.CPF)
+            {
+                Celular = request.Celular,
+                CEP = request.CEP,
+                Rua = request.Rua,
+                Numero = request.Numero,
+                Complemento = request.Complemento,
+                Bairro = request.Bairro,
+                Municipio = request.Municipio,
+                Estado = request.Estado
+            };
+
+            context.Clientes.Add(cliente);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
